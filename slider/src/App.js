ceca2bca3c893ec1e94f25802d60fe8436938ec0
Sajdemon <xxxsajmonxxx@gmail.com>
2024-11-01 09:57:01 +0100

    first commit

---
+++ slider/src/App.js
@@ -0,0 +1,11 @@
+import OneMovieSlider from "./components/oneMovieSlider";
+
+const App = () => {
+  return (
+    <div className="slider">
+      <OneMovieSlider />
+    </div>
+  );
+};
+
+export default App;
---
+++ slider/src/data.js
@@ -0,0 +1,64 @@
+const AllMovies = [
+  {
+    id: 1,
+    image: "./images/movie1.png",
+    title: "Enola Holmesová 2",
+    age: "+13",
+    tags: "Vtipný * Napínavý * Rodina",
+    description:
+      "Enola vyšetřuje svůj první opravdový případ. Rozlousknout záhadu zmizelé dívky jí ale budou muset pomoct kamarádi a taky brácha Sherlock.",
+    category: "komedie",
+  },
+  {
+    id: 2,
+    image: "./images/movie2.png",
+    title: "Enola Holmesová",
+    age: "+13",
+    tags: "Vtipný * Napínavý * Rodina",
+    description:
+      "Při pátrání po nezvěstné matce dokáže neohrožená Enola Holmesová přechytračit i svého staršího bratra Sherlocka a pomoct mladému lordovi na útěku.",
+    category: "komedie",
+  },
+  {
+    id: 3,
+    image: "./images/movie3.png",
+    title: "Rocknrolla",
+    age: "+16",
+    tags: "Gangster * Napínavý * Násilí",
+    description:
+      "Městu vládne mafiánský magnát Lenny Cole, který zde získal nemalé jmění podplácením právníků a pozemkových agentů, od kterých zkupuje ty stavební parcely, které chtějí koupit lidé, jenž si na ně půjčili právě od něj.",
+    category: "romantický",
+  },
+  {
+    id: 4,
+    image: "./images/movie4.png",
+    title: "Patriot",
+    age: "+16",
+    tags: "Gangster * Napínavý * Násilí",
+    description:
+      "V Americe začíná válka o nezávislost, do které se zapojí i jeho nejstarší syn. Nemá proto na výběr a taky vezme zbraň do ruky, aby ochránil to, na čem mu nejvíc záleží.",
+    category: "romantický",
+  },
+  {
+    id: 5,
+    image: "./images/movie5.png",
+    title: "Telefon pana Harrigana",
+    age: "+16",
+    tags: "Gangster * Napínavý * Násilí",
+    description:
+      "Malý kluk a boháč v letech se sblíží díky knihám i svým prvním iPhonům. Jejich záhadné přátelství pak přetrvá i po starcově smrti.",
+    category: "akční",
+  },
+  {
+    id: 6,
+    image: "./images/movie6.png",
+    title: "Stážista",
+    age: "+16",
+    tags: "Rodinný * Napínavý * Romantický",
+    description:
+      "Robert De Niro hraje ve filmu Bena Whittakera, 70letého vdovce, který si uvědomil, že odchod do důchodu není až tak skvělý, jak se říká.",
+    category: "akční",
+  },
+];
+
+export default AllMovies;
